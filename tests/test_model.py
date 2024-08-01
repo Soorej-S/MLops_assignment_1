660a004aa19f491fb0681515f88b51527c4fed82
Soorej-S <soorej.thesun@yahoo.com>
2024-08-01 13:36:37 +0530

    Lint correction 2

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,14 @@
 import numpy as np
 from src.model import SimpleModel
 
+
 def test_model_prediction():
     model = SimpleModel()
     X = np.random.rand(10, 5)
     y = np.random.randint(0, 2, 10)
-    
+
     model.fit(X, y)
     predictions = model.predict(X)
-    
+
     assert len(predictions) == 10
     assert all(isinstance(pred, (int, np.integer)) for pred in predictions)
