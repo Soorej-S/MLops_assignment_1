8d1d622f82268534b7732403106ab97f2065559a
Soorej-S <soorej.thesun@yahoo.com>
2024-08-01 13:41:16 +0530

    Lint correction 4

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,8 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import numpy as np
 from src.model import SimpleModel
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_model_prediction():
     model = SimpleModel()
