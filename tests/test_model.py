9ff72c645d76bdd2ea49941fc1d6b3cbe69c9dba
Soorej-S <soorej.thesun@yahoo.com>
2024-08-01 13:35:00 +0530

    Lint correction

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,15 +1,13 @@
 import numpy as np
-import joblib
-
+from src.model import SimpleModel
 
 def test_model_prediction():
+    model = SimpleModel()
     X = np.random.rand(10, 5)
-
-    # Load the model from the joblib file
-    loaded_model = joblib.load('trained_model.joblib')
-
-    # Make predictions using the loaded model
-    predictions = loaded_model.predict(X)
-
+    y = np.random.randint(0, 2, 10)
+    
+    model.fit(X, y)
+    predictions = model.predict(X)
+    
     assert len(predictions) == 10
     assert all(isinstance(pred, (int, np.integer)) for pred in predictions)
